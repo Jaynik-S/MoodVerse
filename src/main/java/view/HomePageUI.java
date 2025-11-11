620c2b209d2a08aecc059629097d6f47789b5db6
RyanL025 <ryanl041x@163.com>
2025-11-11 12:02:19 -0500

    Created HomePageUI class

---
+++ src/main/java/view/HomePageUI.java
@@ -0,0 +1,22 @@
+package view;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import interface_adapter.note.NoteController;
+import interface_adapter.note.NoteState;
+import interface_adapter.note.NoteViewModel;
+
+public class HomePageUI {
+
+}
