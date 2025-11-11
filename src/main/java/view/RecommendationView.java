f4484e39d2398c349435bb7de6d0fcbe346d743e
espacededavid <toronja.guo@mail.utoronto.ca>
2025-11-11 12:08:38 -0500

    add RecommendationView.java to view

---
+++ src/main/java/view/RecommendationView.java
@@ -0,0 +1,26 @@
+package view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+/**
+ * The View for when the user click on the 'Analyze Mood' button.
+ */
+public class RecommendationView implements ActionListener, PropertyChangeListener {
+
+    /**
+     * React to a button click that results in evt.
+     * @param evt the ActionEvent to react to
+     */
+    public void actionPerformed(ActionEvent evt) {
+        System.out.println("Click " + evt.getActionCommand());
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
+
+}
