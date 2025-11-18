59cc88cee45df11d576966e8c4343d470d3238c1
majinbagel <aidan.wangzh@gmail.com>
2025-11-18 12:01:02 -0500

    lockscreen interface adapters

--- src/main/java/interface_adapter/lock_screen/LockScreenPresenter.java
+++ src/main/java/interface_adapter/lock_screen/LockScreenPresenter.java
@@ -1,5 +1,29 @@
 package interface_adapter.lock_screen;
 
-public class LockScreenPresenter {
+import interface_adapter.ViewModel;
+import use_case.verify_password.VerifyPasswordOutputBoundary;
+import use_case.verify_password.VerifyPasswordOutputData;
+
+public class LockScreenPresenter implements VerifyPasswordOutputBoundary{
+
+    private LockScreenViewModel lockScreenViewModel;
+
+    public LockScreenPresenter(LockScreenViewModel lockScreenViewModel) {
+        this.lockScreenViewModel = lockScreenViewModel;
+    }
+
+    public void prepareSuccessView(VerifyPasswordOutputData outputData){
+        final LockScreenState state = lockScreenViewModel.getState();
+        state.setError(null);
+        lockScreenViewModel.firePropertyChanged();
+    }
+
+    public void prepareFailView(String errorMessage){
+        final LockScreenState state = lockScreenViewModel.getState();
+        state.setError(errorMessage);
+        lockScreenViewModel.firePropertyChanged();
+    }
 }
+
+
 
