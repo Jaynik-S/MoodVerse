5d010c8775f38e1d9ca149b3ac6ac1726e78bc36
maniic <abdullahchabaytah@gmail.com>
2025-11-18 12:25:37 -0500

    Implement CreateEntryInputData

--- src/main/java/use_case/create_entry/CreateEntryInputData.java
+++ src/main/java/use_case/create_entry/CreateEntryInputData.java
@@ -1,5 +1,18 @@
 package use_case.create_entry;
+import java.util.List;
 
 public class CreateEntryInputData {
+    private String title;
+    private String text;
+    private List<String> keywords;
+
+    public CreateEntryInputData(String title, String text, List<String> keywords) {
+        this.title = title;
+        this.text = text;
+        this.keywords = keywords;
+    }
+    public String getTitle() { return title; }
+    public String getText() { return text; }
+    public List<String> getKeywords() { return keywords; }
 }
 
