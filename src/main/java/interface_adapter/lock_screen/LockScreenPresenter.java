889241db52b64a0cc67a574c33f1adcc6655c4b4
majinbagel <aidan.wangzh@gmail.com>
2025-11-18 14:47:07 -0500

    updated presenter

--- src/main/java/interface_adapter/lock_screen/LockScreenPresenter.java
+++ src/main/java/interface_adapter/lock_screen/LockScreenPresenter.java
@@ -1,21 +1,29 @@
 package interface_adapter.lock_screen;
 
-import interface_adapter.ViewModel;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.home_menu.HomeMenuViewModel;
 import use_case.verify_password.VerifyPasswordOutputBoundary;
 import use_case.verify_password.VerifyPasswordOutputData;
 
 public class LockScreenPresenter implements VerifyPasswordOutputBoundary{
 
-    private LockScreenViewModel lockScreenViewModel;
+    private final LockScreenViewModel lockScreenViewModel;
+    private final ViewManagerModel viewManagerModel;
+    private final HomeMenuViewModel homeMenuViewModel;
 
-    public LockScreenPresenter(LockScreenViewModel lockScreenViewModel) {
+    public LockScreenPresenter(LockScreenViewModel lockScreenViewModel, ViewManagerModel viewManagerModel, HomeMenuViewModel homeMenuViewModel) {
         this.lockScreenViewModel = lockScreenViewModel;
+        this.viewManagerModel = viewManagerModel;
+        this.homeMenuViewModel = homeMenuViewModel;
     }
 
     public void prepareSuccessView(VerifyPasswordOutputData outputData){
         final LockScreenState state = lockScreenViewModel.getState();
         state.setError(null);
         lockScreenViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setState(homeMenuViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
     }
 
     public void prepareFailView(String errorMessage){
