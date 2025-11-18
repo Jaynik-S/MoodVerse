dc3c2dc8e3dba6373226e0d22134e5820f7cf713
maniic <abdullahchabaytah@gmail.com>
2025-11-18 12:31:12 -0500

    Implement CreateEntryOutputData

--- src/main/java/use_case/create_entry/CreateEntryOutputData.java
+++ src/main/java/use_case/create_entry/CreateEntryOutputData.java
@@ -1,5 +1,22 @@
 package use_case.create_entry;
 
+import java.time.LocalDateTime;
+
 public class CreateEntryOutputData {
+    private final int entryId;
+    private final String title;
+    private final String textPreview;
+    private final LocalDateTime createdAt;
+
+    public CreateEntryOutputData(int entryId, String title, String textPreview, LocalDateTime createdAt) {
+        this.entryId = entryId;
+        this.title = title;
+        this.textPreview = textPreview;
+        this.createdAt = createdAt;
+    }
+    public int getEntryId() { return entryId; }
+    public String getTitle() { return title; }
+    public String getTextPreview() { return textPreview; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
 }
 
