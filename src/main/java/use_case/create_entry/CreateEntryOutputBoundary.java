70870d61af75566911f20029c6bc637fe5439408
maniic <abdullahchabaytah@gmail.com>
2025-11-18 12:35:16 -0500

    Create CreateEntryOutputBoundary

--- src/main/java/use_case/create_entry/CreateEntryOutputBoundary.java
+++ src/main/java/use_case/create_entry/CreateEntryOutputBoundary.java
@@ -1,4 +1,6 @@
 package use_case.create_entry;
 
 public interface CreateEntryOutputBoundary {
+    void prepareSuccessView(CreateEntryOutputData outputData);
+    void prepareFailView(String errorMessage);
 }
