e657b706558dd734e53dcfbecda24d4506411601
majinbagel <aidan.wangzh@gmail.com>
2025-11-18 15:11:55 -0500

    Updated GoBackPresenter.java

--- src/main/java/interface_adapter/GoBackPresenter.java
+++ src/main/java/interface_adapter/GoBackPresenter.java
@@ -1,18 +1,21 @@
 package interface_adapter;
 
+import interface_adapter.home_menu.HomeMenuViewModel;
 import interface_adapter.ViewManagerModel;
 import use_case.go_back.GoBackOutputBoundary;
 
 public class GoBackPresenter implements GoBackOutputBoundary{
 
     private final ViewManagerModel viewManagerModel;
+    private final HomeMenuModel homeMenuViewModel;
 
-    public GoBackPresenter(ViewManagerModel viewManagerModel) {
+    public GoBackPresenter(ViewManagerModel viewManagerModel, HomeMenuViewModel homeMenuViewModel) {
         this.viewManagerModel = viewManagerModel;
+        this.homeMenuViewModel = homeMenuViewModel;
     }
     @Override
     public void prepareSuccessView() {
-        viewManagerModel.setState("Home");
+        viewManagerModel.setState(homeMenuViewModel.geViewName());
         viewManagerModel.firePropertyChanged();
     }
 }
