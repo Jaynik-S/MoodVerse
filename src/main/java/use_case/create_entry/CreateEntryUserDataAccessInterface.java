f2de194d5ab3a085fed9e6b14a6b7798c13d9799
Abdullah Chabaytah <Abdullahchabaytah@gmail.com>
2025-11-18 21:20:18 -0500

    Remove CreateEntryUserDataAccessInterface interface

--- src/main/java/use_case/create_entry/CreateEntryUserDataAccessInterface.java
+++ src/main/java/use_case/create_entry/CreateEntryUserDataAccessInterface.java
@@ -1,9 +1,1 @@
-package use_case.create_entry;
 
-import entity.DiaryEntry;
-
-public interface CreateEntryUserDataAccessInterface {
-    void save(DiaryEntry entry);
-
-    boolean existsById(int entryId);
-}
