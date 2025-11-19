707d195b9b9ed19213b9f84b793a7738e6643b69
maniic <abdullahchabaytah@gmail.com>
2025-11-19 18:55:29 -0500

    Implement SaveEntryOutputBoundary

--- src/main/java/use_case/save_entry/SaveEntryOutputBoundary.java
+++ src/main/java/use_case/save_entry/SaveEntryOutputBoundary.java
@@ -1,5 +1,9 @@
 package use_case.save_entry;
 
+import entity.DiaryEntry;
+
 public interface SaveEntryOutputBoundary {
+    void prepareSuccessView(DiaryEntry savedEntry);
+    void prepareFailureView(String errorMessage);
 }
 
