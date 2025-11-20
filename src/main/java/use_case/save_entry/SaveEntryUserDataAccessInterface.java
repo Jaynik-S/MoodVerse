1606b9eaa56f2c65dd91421a8bb20e7c47389075
maniic <abdullahchabaytah@gmail.com>
2025-11-19 23:46:03 -0500

    Add error handling

--- src/main/java/use_case/save_entry/SaveEntryUserDataAccessInterface.java
+++ src/main/java/use_case/save_entry/SaveEntryUserDataAccessInterface.java
@@ -2,7 +2,9 @@ package use_case.save_entry;
 
 import entity.DiaryEntry;
 
+import java.io.IOException;
+
 public interface SaveEntryUserDataAccessInterface {
-    void save(DiaryEntry entry);
+    void save(DiaryEntry entry) throws IOException, InterruptedException;
 }
 
