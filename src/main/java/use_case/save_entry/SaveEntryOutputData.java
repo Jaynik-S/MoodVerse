612917f076c3e8a6fa5cabbb2c200b9c333e5507
maniic <abdullahchabaytah@gmail.com>
2025-11-19 23:25:55 -0500

    Refactor

--- src/main/java/use_case/save_entry/SaveEntryOutputData.java
+++ src/main/java/use_case/save_entry/SaveEntryOutputData.java
@@ -5,19 +5,14 @@ import entity.DiaryEntry;
 public class SaveEntryOutputData {
 
     private final DiaryEntry entry;
-    private final String entryPath;
     private final boolean success;
-    public SaveEntryOutputData(DiaryEntry entry, String entryPath, boolean success) {
+    public SaveEntryOutputData(DiaryEntry entry, boolean success) {
         this.entry = entry;
-        this.entryPath = entryPath;
         this.success = success;
     }
 
     public DiaryEntry getEntry() {
         return entry;
-    }
-    public String getEntryPath() {
-        return entryPath;
     }
     public boolean isSuccess() {
         return success;
