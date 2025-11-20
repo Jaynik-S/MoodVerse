8a2fe39c8f56f42b117f22896e393bf2cf6a43eb
maniic <abdullahchabaytah@gmail.com>
2025-11-19 21:36:14 -0500

    Implement SaveEntryInputData

--- src/main/java/use_case/save_entry/SaveEntryInputData.java
+++ src/main/java/use_case/save_entry/SaveEntryInputData.java
@@ -1,5 +1,21 @@
 package use_case.save_entry;
 
+import entity.DiaryEntry;
+
 public class SaveEntryInputData {
+    private final DiaryEntry entry;
+    private final String entryPath;
+
+    public SaveEntryInputData(DiaryEntry entry, String entryPath) {
+        this.entry = entry;
+        this.entryPath = entryPath;
+    }
+
+    public DiaryEntry getEntry() {
+        return entry;
+    }
+    public String getEntryPath() {
+        return entryPath;
+    }
 }
 
