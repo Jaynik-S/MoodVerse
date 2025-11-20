74d0d49b5bb36f5b71bf8c6bf375b5135ab3ed43
maniic <abdullahchabaytah@gmail.com>
2025-11-20 13:56:48 -0500

    Implement OutputData

--- src/main/java/use_case/delete_entry/DeleteEntryOutputData.java
+++ src/main/java/use_case/delete_entry/DeleteEntryOutputData.java
@@ -1,5 +1,14 @@
 package use_case.delete_entry;
 
 public class DeleteEntryOutputData {
+    private final String entryPath;
+    private final boolean success;
+
+    public DeleteEntryOutputData(String entryPath, boolean success) {
+        this.entryPath = entryPath;
+        this.success = success;
+    }
+    public String getEntryPath() { return entryPath; }
+    public boolean isSuccess() { return success; }
 }
 
