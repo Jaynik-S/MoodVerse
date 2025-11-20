4ce8147bdc6e3cac5e4a66603f5ff4c91a60fec8
maniic <abdullahchabaytah@gmail.com>
2025-11-20 01:35:18 -0500

    Implement OutputBoundary

--- src/main/java/use_case/load_entry/LoadEntryOutputBoundary.java
+++ src/main/java/use_case/load_entry/LoadEntryOutputBoundary.java
@@ -1,4 +1,8 @@
 package use_case.load_entry;
 
+import java.io.IOException;
+
 public interface LoadEntryOutputBoundary {
+    void prepareSuccessView(LoadEntryInputData outputData);
+    void prepareFailureView(String errorMessage);
 }
