47e7ae7ecd8203f0287b35ea9d010779a6943afe
maniic <abdullahchabaytah@gmail.com>
2025-11-19 22:44:59 -0500

    Remove unnecessary files

--- src/main/java/use_case/create_entry/CreateEntryUserDataAccessInterface.java
+++
@@ -1,8 +0,0 @@
-package use_case.create_entry;
-
-import entity.DiaryEntry;
-
-public interface CreateEntryUserDataAccessInterface {
-    void save(DiaryEntry entry);
-    boolean existsById(int entryId);
-}
