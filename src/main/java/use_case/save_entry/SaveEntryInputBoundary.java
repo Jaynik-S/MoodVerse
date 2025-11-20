7a782b264252456d4297817b1910d94754abfbdc
maniic <abdullahchabaytah@gmail.com>
2025-11-19 21:52:37 -0500

    Fix parameters

--- src/main/java/use_case/save_entry/SaveEntryInputBoundary.java
+++ src/main/java/use_case/save_entry/SaveEntryInputBoundary.java
@@ -1,7 +1,5 @@
 package use_case.save_entry;
 
-import entity.DiaryEntry;
-
 public interface SaveEntryInputBoundary {
-    void execute(DiaryEntry entry);
+    void execute(SaveEntryInputData inputData);
 }
