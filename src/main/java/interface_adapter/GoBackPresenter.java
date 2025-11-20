9061980947183487b5de80445de2dd69b70949d8
majinbagel <aidan.wangzh@gmail.com>
2025-11-20 16:40:51 -0500

    Fixed typo in GoBackPresenter

--- src/main/java/interface_adapter/GoBackPresenter.java
+++ src/main/java/interface_adapter/GoBackPresenter.java
@@ -1,13 +1,12 @@
 package interface_adapter;
 
 import interface_adapter.home_menu.HomeMenuViewModel;
-import interface_adapter.ViewManagerModel;
 import use_case.go_back.GoBackOutputBoundary;
 
 public class GoBackPresenter implements GoBackOutputBoundary{
 
     private final ViewManagerModel viewManagerModel;
-    private final HomeMenuModel homeMenuViewModel;
+    private final HomeMenuViewModel homeMenuViewModel;
 
     public GoBackPresenter(ViewManagerModel viewManagerModel, HomeMenuViewModel homeMenuViewModel) {
         this.viewManagerModel = viewManagerModel;
