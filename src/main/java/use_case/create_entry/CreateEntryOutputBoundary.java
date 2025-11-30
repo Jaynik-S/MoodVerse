922bdaf3b80ad80b9c2d70af71d4e0d7091b8199
maniic <abdullahchabaytah@gmail.com>
2025-11-30 05:42:12 -0500

    Remove unused import

--- src/main/java/use_case/create_entry/CreateEntryOutputBoundary.java
+++ src/main/java/use_case/create_entry/CreateEntryOutputBoundary.java
@@ -1,7 +1,5 @@
 package use_case.create_entry;
 
-import entity.DiaryEntry;
-
 public interface CreateEntryOutputBoundary {
     void prepareSuccessView(CreateEntryOutputData outputData);
     void prepareFailView(String errorMessage);
